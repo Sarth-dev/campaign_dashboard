a10958be8db3c562354fb61e9a6226e521700ece
Sarth-dev <sarthakkale2003@gmail.com>
2025-12-17 14:03:47 +0530

    final fix

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,8 +1,9 @@
-
+from django.http import JsonResponse
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', lambda request: JsonResponse({"status": "API running"})),
     path('admin/', admin.site.urls),
     path('api/', include('backend.campaigns.urls')),
 ]
