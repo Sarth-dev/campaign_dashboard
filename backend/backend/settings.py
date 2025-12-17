4314e92075c03385f0ff78bf1e6905808203863d
Sarth-dev <sarthakkale2003@gmail.com>
2025-12-17 18:35:39 +0530

    DB solve

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -93,10 +93,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
+    "default": dj_database_url.config(
+        default=f"postgres://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}",
         conn_max_age=600,
-        ssl_require=True
+        ssl_require=True,
     )
 }
 
